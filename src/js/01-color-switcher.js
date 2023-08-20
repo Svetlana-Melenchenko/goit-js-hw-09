06103ddff750ca74ae3288cdff2d3afc0445ee7b
Svetlana-Melenchenko <129314558+Svetlana-Melenchenko@users.noreply.github.com>
2023-08-20 13:18:58 +0300

    task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+
+let intervalId = null;
+
+function changeBackgroundColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+startButton.addEventListener('click', () => {
+  if (intervalId === null) {
+    intervalId = setInterval(changeBackgroundColor, 1000);
+    startButton.disabled = true;
+  }
+});
+
+stopButton.addEventListener('click', () => {
+  if (intervalId !== null) {
+    clearInterval(intervalId);
+    intervalId = null;
+    startButton.disabled = false;
+  }
+});
